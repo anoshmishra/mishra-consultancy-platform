c9368d63371632c4b62d00d0d00e9a987f88b297
Anosh mishra <75946348+anoshmishra@users.noreply.github.com>
2026-04-07 21:54:41 +0530

    Fix middleware import typo

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
